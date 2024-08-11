968bc35c7601706e809e4951321c5211585419ee
Derek Bal <bal.derek.j@gmail.com>
2024-08-11 07:49:02 -1000

    all dropdown better as details

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,22 @@ export default {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["lemonade", "dim"],
+    themes: [
+      "lemonade",
+      "dim",
+      {
+        custom: {
+          primary: "#76b82a",
+          secondary: "#9333ea",
+          accent: "#ff6f00",
+          neutral: "#d1c4e9",
+          "base-100": "#fefeff",
+          info: "#1d4ed8",
+          success: "#4caf50",
+          warning: "#fcd34d",
+          error: "#d32f2f",
+        },
+      },
+    ],
   },
 };
