9713086a9a59ff3818369dddbba65b0dd4044d91
Derek Bal <bal.derek.j@gmail.com>
2024-08-12 06:53:15 -1000

    checkpoint

---
+++ src/specials/announcements.md
@@ -0,0 +1,7 @@
+---
+title: Aloha!
+---
+
+Registration for the Native Hawaiian Education Association Conference this March 21 and 22, 2024 is now open! View details [here](#). Register [here](#).
+
+Mahalo!
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: [
       {
@@ -12,20 +12,23 @@ export default {
           primary: "#76b82a",
           secondary: "#9333ea",
           accent: "#ff6f00",
-          neutral: "#d1c4e9",
+          neutral: "#c0c0c0",
           "base-100": "#fefeff",
           info: "#1d4ed8",
           success: "#4caf50",
           warning: "#fcd34d",
           error: "#d32f2f",
+          "--opposite": "#15181e",
         },
       },
       {
         dim: {
           ...require("daisyui/src/theming/themes")["dim"],
           "--b1": "20.85% 0.013 264.24",
+          "--opposite": "#fefeff",
         },
       },
     ],
   },
+  darkMode: ["class", 'html[data-theme="dim"]'],
 };
